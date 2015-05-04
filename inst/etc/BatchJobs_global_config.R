6e1166a9d82fc48847933ba60112be5dcefcbe14
Dan Bottomly <bottomly@ohsu.edu>
2015-05-04 12:24:29 -0700

    ensured that staged.queries are set to T, for /mnt/tempwork

--- inst/etc/BatchJobs_global_config.R
+++ inst/etc/BatchJobs_global_config.R
@@ -6,3 +6,5 @@ db.driver = "SQLite"
 default.resources <- list(universe='vanilla', R.bin.path=file.path(dirname(dirname(find.package("BatchJobs"))), "bin", "R"), memory='5 GB', cpus='1')
 db.options = list()
 debug = FALSE
+#this should ensure that there is no issue with NFS locking if run on /mnt/tempwork 
+staged.queries = TRUE
