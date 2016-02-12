5a59b70b5c7a1b378a63e2fa8200e4def3bf961a
Dan Bottomly <bottomly@ohsu.edu>
2016-02-12 13:35:00 -0800

    add db.options
    added db.options to avoid db locks, should be safe as long as staged.queries = TRUE

--- inst/etc/BatchJobs_global_config.R
+++ inst/etc/BatchJobs_global_config.R
@@ -4,7 +4,8 @@ mail.done = "none"
 mail.error = "none"
 db.driver = "SQLite"
 default.resources <- list(universe='vanilla', R.bin.path=file.path(dirname(dirname(find.package("BatchJobs"))), "bin", "R"), memory='5 GB', cpus='1')
-db.options = list()
+#as suggested here https://github.com/tudo-r/BatchJobs/wiki/Configuration
+db.options = list(pragmas = c("busy_timeout=5000", "journal_mode=WAL"))
 debug = FALSE
 #this should ensure that there is no issue with NFS locking if run on /mnt/tempwork 
 staged.queries = TRUE
