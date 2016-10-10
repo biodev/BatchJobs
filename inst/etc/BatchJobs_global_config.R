bab0bd8469d0e1d9c127c72efd7b896c68e8e9eb
Daniel Bottomly <bottomly@hrcc448.ohsu.edu>
2016-10-10 10:06:17 -0700

    need R to be full path

--- inst/etc/BatchJobs_global_config.R
+++ inst/etc/BatchJobs_global_config.R
@@ -3,7 +3,7 @@ mail.start = "none"
 mail.done = "none"
 mail.error = "none"
 db.driver = "SQLite"
-default.resources <- list(universe='vanilla', R.bin.path=ifelse(as.character(Sys.info()["nodename"]) == "morgan2.ohsu.edu", file.path(dirname(dirname(find.package("BatchJobs"))), "bin", "R"), "R"), memory='5 GB', cpus='1', walltime=10*60)
+default.resources <- list(universe='vanilla', R.bin.path=ifelse(as.character(Sys.info()["nodename"]) == "morgan2.ohsu.edu", file.path(dirname(dirname(find.package("BatchJobs"))), "bin", "R"), "/usr/bin/R"), memory='5 GB', cpus='1', walltime=10*60)
 #as suggested here https://github.com/tudo-r/BatchJobs/wiki/Configuration
 db.options = list(pragmas = c("busy_timeout=5000", "journal_mode=WAL"))
 debug = FALSE
